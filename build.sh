7a981ac606ac8938e555ab8e82608bb220265271
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-06-21 23:01:20 +0530

    Setup travis (#1)
    * Enable sudo
    
    * Do not require sudo to install hugo
    
    Use the zip instead of the .deb package.
    
    * Setup hugo in before_script
    
    * Call the deploy script manually
    
    * Generate the service worker after building the hugo site
    
    * Download to the ~ directory instead
    
    * Specify the output file
    
    * List files in the extracted directory
    
    * Specify the file while extracting
    
    * Extract to ~
    
    * Fix tar command
    
    * Get the paths clean
    
    * Fix typo 🤷🏾‍♂️
    
    * Do not run git clean on travis
    
    It's not required there.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 yarn
+hugo --baseURL https://hymnsrepo.github.io/hymnsrepo-hugo
 gulp generate-sw
 mv service-worker.js public/
-hugo --baseURL https://hymnsrepo.github.io/hymnsrepo-hugo
 mkdir ./public/api
 yarn generate-apis
