971de7ce8d07349aa6f8c2c743d97f22a0f03714
Galdin Raphael <gldraphael@gmail.com>
2020-01-12 00:01:41 +0530

    Replace sw-precache with workbox-build
    Since sw-precache is deprecated.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 const gulp = require('gulp');
-const path = require('path');
-const swPrecache = require('sw-precache');
+workbox = require('workbox-build');
 
-gulp.task('generate-sw', function(callback) {
-  swPrecache.write('service-worker.js', {
-    staticFileGlobs: ['public/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2}'],
-    stripPrefix: "public",
-    navigateFallback: '/'
-  }, callback);
+gulp.task('generate-sw', () => {
+  return workbox.generateSW({
+    globDirectory: 'public',
+    globPatterns: [
+      '**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2}'
+    ],
+    swDest: 'public/sw.js',
+    clientsClaim: true,
+    skipWaiting: true
+  }).then(({warnings}) => {
+    // In case there are any warnings from workbox-build, log them.
+    for (const warning of warnings) {
+      console.warn(warning);
+    }
+    console.info('Service worker generation completed.');
+  }).catch((error) => {
+    console.warn('Service worker generation failed:', error);
+  });
 });
